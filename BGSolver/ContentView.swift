3c475fce25f2e5d2968d17a8a5aa5422029f1918
Igor Podolsiy <delusionhs@mail.ru>
2020-09-11 20:40:40 +0200

    Initial Commit

---
+++ BGSolver/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BGSolver
+//
+//  Created by Igor Podolskiy on 11.09.2020.
+//  Copyright © 2020 Igor Podolskiy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
