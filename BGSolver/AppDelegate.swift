b159d4e1c30f0a46920a8dde011f93cfde1ac675
Igor Podolsiy <delusionhs@mail.ru>
2020-09-14 19:37:49 +0200

    add battle simulator for current functional

--- BGSolver/AppDelegate.swift
+++ BGSolver/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = UnitCell(unit: EntityViewModel(unit: Entity()))
+        let contentView = MainView()
 
         // Create the window and set the content view. 
         window = NSWindow(
--- BGSolver/UI/Views/MainView.swift
+++ BGSolver/UI/Views/MainView.swift
@@ -10,39 +10,62 @@ import SwiftUI
 
 struct MainView: View {
 
+    let bottomBoardView = BoardView()
+    let topBoardView = BoardView()
+
     @State var resultText = "WIN: 0% LOSE: 0% TIE:0%"
 
     var body: some View {
         VStack {
             HStack {
-                BoardView()
+                topBoardView
             }
             HStack {
                 Button(action: {
-                    self.resultText  = self.testBattle()
+                    self.resultText  = self.startBattle(bottomBoard: self.bottomBoardView.getBoard(), topBoard: self.topBoardView.getBoard())
+                  //  self.resultText  = self.testBattle()
                 }) {
                 Text("Start Battle")
                 }
                 Text(resultText)
             }
             HStack {
-                BoardView()
+                bottomBoardView
             }
         }
     }
 }
 
 struct BoardView: View {
+    let UnitSlot1 = UnitCell()
+    let UnitSlot2 = UnitCell()
+    let UnitSlot3 = UnitCell()
+    let UnitSlot4 = UnitCell()
+    let UnitSlot5 = UnitCell()
+    let UnitSlot6 = UnitCell()
+    let UnitSlot7 = UnitCell()
+
+
     var body: some View {
         HStack {
-            UnitCell()
-            UnitCell()
-            UnitCell()
-            UnitCell()
-            UnitCell()
-            UnitCell()
-            UnitCell()
+            UnitSlot1
+            UnitSlot2
+            UnitSlot3
+            UnitSlot4
+            UnitSlot5
+            UnitSlot6
+            UnitSlot7
         }
+    }
+
+    func getBoard() -> Board {
+        let board = Board()
+        for unitSlot in [UnitSlot1,UnitSlot2,UnitSlot3,UnitSlot4,UnitSlot5,UnitSlot6,UnitSlot7] {
+            if let unit = unitSlot.unit.unit {
+                board.addUnit(unit: unit)
+            }
+        }
+        return board
     }
 }
 
@@ -53,9 +76,17 @@ struct Main_Previews: PreviewProvider {
 }
 
 extension MainView {
+    func startBattle(bottomBoard: Board, topBoard: Board) -> String {
+        let BG = BattleGround(playerBoard: bottomBoard, enemyBoard: topBoard)
+        for _ in 0..<100{
+            BG.startBattle()
+        }
+        return(BG.returnResultInPercent())
+    }
+
     func testBattle() -> String {
 
-        var myBoard = Board()
+        let myBoard = Board()
 
         myBoard.units = [Entity(race: .murloc, attack: 2, health: 1),Entity(race: .murloc, attack: 2, health: 1),Entity(race: .murloc, attack: 1, health: 1),
                          Entity(attack: 2, health: 2, mechanics: [DeathRattleMechanics(withDamageForOppositeBoard: 4), RebornMechanics(RebornedUnit: Entity(attack: 2, health: 1, mechanics: [DeathRattleMechanics(withDamageForOppositeBoard: 4)]))]),
@@ -64,7 +95,7 @@ extension MainView {
                          Entity(race: .murloc, attack: 3, health: 3, mechanics: [AurasBuffMechanics(forRace: .murloc, attackBuffCount: 2, healthBuffCount: 0)])
         ]
 
-        var opponentBoard = Board()
+        let opponentBoard = Board()
 
         opponentBoard.units = [Entity(attack: 4, health: 6, isTaunt: true),
                                Entity(attack: 2, health: 3, mechanics: [DeathRattleMechanics(withSummonedUnit: Entity(attack: 2, health: 1))]),
@@ -74,9 +105,9 @@ extension MainView {
 
         ]
 
-        var BG = BattleGround(playerBoard: myBoard, enemyBoard: opponentBoard)
+        let BG = BattleGround(playerBoard: myBoard, enemyBoard: opponentBoard)
 
-        for i in 0..<100{
+        for _ in 0..<100{
             BG.startBattle()
         }
 
